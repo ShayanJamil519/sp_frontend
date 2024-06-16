311edef6e3334f0960bf3469d375cbf80b515b4d
M-Abdullah-Bhatti <87266652+M-Abdullah-Bhatti@users.noreply.github.com>
2024-06-16 18:39:28 +0500

    credit score graph done

--- src/components/Dashboard/Simulation/CreditScore/ChartOne.jsx
+++ src/components/Dashboard/Simulation/CreditScore/ChartOne.jsx
@@ -1,134 +1,97 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import ReactApexChart from "react-apexcharts";
 
-const options = {
-  legend: {
-    show: false,
-    position: "top",
-    horizontalAlign: "left",
-  },
-  colors: ["#3C50E0", "#80CAEE"],
-  chart: {
-    fontFamily: "Satoshi, sans-serif",
-    height: 335,
-    type: "area",
-    dropShadow: {
-      enabled: true,
-      color: "#623CEA14",
-      top: 10,
-      blur: 4,
-      left: 0,
-      opacity: 0.1,
-    },
-
-    toolbar: {
-      show: false,
-    },
-  },
-  responsive: [
-    {
-      breakpoint: 1024,
-      options: {
-        chart: {
-          height: 300,
-        },
-      },
-    },
-    {
-      breakpoint: 1366,
-      options: {
-        chart: {
-          height: 350,
-        },
-      },
-    },
-  ],
-  stroke: {
-    width: [2, 2],
-    curve: "straight",
-  },
-  // labels: {
-  //   show: false,
-  //   position: "top",
-  // },
-  grid: {
-    xaxis: {
-      lines: {
-        show: true,
-      },
-    },
-    yaxis: {
-      lines: {
-        show: true,
-      },
-    },
-  },
-  dataLabels: {
-    enabled: false,
-  },
-  markers: {
-    size: 4,
-    colors: "#fff",
-    strokeColors: ["#3056D3", "#80CAEE"],
-    strokeWidth: 3,
-    strokeOpacity: 0.9,
-    strokeDashArray: 0,
-    fillOpacity: 1,
-    discrete: [],
-    hover: {
-      size: undefined,
-      sizeOffset: 5,
-    },
-  },
-  xaxis: {
-    type: "category",
-    categories: [
-      "Sep",
-      "Oct",
-      "Nov",
-      "Dec",
-      "Jan",
-      "Feb",
-      "Mar",
-      "Apr",
-      "May",
-      "Jun",
-      "Jul",
-      "Aug",
-    ],
-    axisBorder: {
-      show: false,
-    },
-    axisTicks: {
-      show: false,
-    },
-  },
-  yaxis: {
-    title: {
-      style: {
-        fontSize: "0px",
-      },
-    },
-    min: 0,
-    max: 100,
-  },
-};
-
-const ChartOne = () => {
+const ChartOne = ({ data }) => {
+  console.log("data: ", data);
   const [state, setState] = useState({
     series: [
       {
-        name: "Complaints Resolved",
-        data: [23, 11, 22, 27, 13, 22, 37, 21, 44, 22, 30, 45],
-      },
-
-      {
-        name: "Complaints Made",
-        data: [30, 25, 36, 30, 45, 35, 64, 52, 59, 36, 39, 51],
+        name: "Desktops",
+        data: [301, 340, 435, 464, 836, 345, 454, 340, 454, 765, 811],
       },
     ],
+    responsive: [
+      {
+        breakpoint: 1024,
+        options: {
+          chart: {
+            height: 300,
+          },
+        },
+      },
+      {
+        breakpoint: 1366,
+        options: {
+          chart: {
+            height: 350,
+          },
+        },
+      },
+    ],
+    options: {
+      chart: {
+        height: 350,
+        type: "line",
+        zoom: {
+          enabled: false,
+        },
+      },
+      dataLabels: {
+        enabled: false,
+      },
+      stroke: {
+        curve: "straight",
+      },
+      title: {
+        text: "Credit Score",
+        align: "left",
+      },
+      grid: {
+        row: {
+          colors: ["#f3f3f3", "transparent"], // takes an array which will be repeated on columns
+          opacity: 0.5,
+        },
+      },
+
+      markers: {
+        size: 4,
+        colors: "#fff",
+        strokeColors: ["#3056D3", "#80CAEE"],
+        strokeWidth: 3,
+        strokeOpacity: 0.9,
+        strokeDashArray: 0,
+        fillOpacity: 1,
+        discrete: [],
+        hover: {
+          size: undefined,
+          sizeOffset: 5,
+        },
+      },
+      xaxis: {
+        type: "numeric",
+        title: {
+          text: "Default Probability (%)",
+        },
+        tickAmount: 10,
+        min: 0, // Define a minimum if applicable
+        max: 10, // Define a maximum based on expected probability range * 100
+        labels: {
+          formatter: (val) => `${parseFloat(val).toFixed(2)}%`, // Ensure labels show significant digits
+        },
+      },
+      yaxis: {
+        title: {
+          style: {
+            fontSize: "0px",
+          },
+        },
+        min: 300,
+        max: 850,
+      },
+    },
+    series: [],
   });
 
   const handleReset = () => {
@@ -139,33 +102,28 @@ const ChartOne = () => {
 
   handleReset;
 
+  useEffect(() => {
+    if (Array.isArray(data)) {
+      const formattedData = data.map((d) => ({
+        x: d.probability * 100, // Convert probability to percentage
+        y: d.score,
+      }));
+      console.log("Formatted data for chart:", formattedData);
+      setState((prevState) => ({
+        ...prevState,
+        series: [{ name: "Credit Score", data: formattedData }],
+      }));
+    }
+  }, [data]);
+
   return (
     <div className="col-span-12 rounded-sm border border-stroke bg-white px-5 pt-7 pb-5 shadow-sm sm:px-7 xl:col-span-8">
-      <div className="flex flex-wrap items-start justify-between gap-3 sm:flex-nowrap">
-        <div className="flex w-full flex-wrap gap-3 sm:gap-5">
-          <div></div>
-        </div>
-        <div className="flex w-full max-w-45 justify-end">
-          <div className="inline-flex items-center rounded-md bg-[#f5f7fd] p-1 ">
-            <button className="rounded bg-white py-1 px-3 text-xs font-medium text-black shadow-card hover:bg-white ">
-              Day
-            </button>
-            <button className="rounded py-1 px-3 text-xs font-medium text-black hover:bg-white ">
-              Week
-            </button>
-            <button className="rounded py-1 px-3 text-xs font-medium text-black hover:bg-white ">
-              Month
-            </button>
-          </div>
-        </div>
-      </div>
-
       <div>
-        <div id="chartOne" className="-ml-5 h-[355px] w-[105%]">
+        <div id="chartOne" className="-ml-5 h-[500px] w-[103%]">
           <ReactApexChart
-            options={options}
+            options={state.options}
             series={state.series}
-            type="area"
+            type="line"
             width="100%"
             height="100%"
           />
--- src/components/Dashboard/Simulation/CreditScore/index.jsx
+++ src/components/Dashboard/Simulation/CreditScore/index.jsx
@@ -1,4 +1,7 @@
+"use client";
+import axios from "axios";
 import dynamic from "next/dynamic";
+import { useEffect, useState } from "react";
 
 const ChartOne = dynamic(() => import("./ChartOne"), {
   ssr: false,
@@ -13,16 +16,50 @@ const ChartThree = dynamic(() => import("./ChartThree"), {
 });
 
 export const CreditScore = () => {
+  const [creditScore, setCreditScore] = useState(850);
+  const [defaultProbability, setDefaultProbability] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.post(
+          "https://stochastic-process.vercel.app/simulate",
+          { credit_score: parseInt(creditScore) }
+        );
+        setDefaultProbability((prev) => [
+          ...prev,
+          {
+            score: parseInt(creditScore), // Make sure the score is a number
+            probability: response.data.default_probability,
+          },
+        ]);
+      } catch (error) {
+        console.error("API call error:", error.response);
+      }
+    };
+
+    fetchData();
+  }, [creditScore]);
+
   return (
     <div className="">
-      <div className="mt-4 grid grid-cols-12 gap-4 md:mt-6 md:gap-6 2xl:mt-7 2xl:gap-7">
+      <input
+        type="range"
+        min="300"
+        max="850"
+        value={creditScore}
+        onChange={(e) => setCreditScore(e.target.value)}
+      />
+
+      <ChartOne data={defaultProbability} />
+      {/* <div className="mt-4 grid grid-cols-12 gap-4 md:mt-6 md:gap-6 2xl:mt-7 2xl:gap-7">
         <ChartOne />
 
         <ChartTwo />
         <div className="col-span-12 xl:col-span-12 w-full">
           <ChartThree />
         </div>
-      </div>
+      </div> */}
     </div>
   );
 };
