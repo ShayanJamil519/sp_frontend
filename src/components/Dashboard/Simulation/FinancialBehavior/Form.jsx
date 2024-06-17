0b1adc5b0ff7e52eff0fe696e971fe0e10fb189f
ShayanJamil519 <shayanjamil500@gmail.com>
2024-06-18 00:02:26 +0500

    Forms Added

--- src/components/Dashboard/Simulation/FinancialBehavior/Form.jsx
+++ src/components/Dashboard/Simulation/FinancialBehavior/Form.jsx
@@ -11,18 +11,12 @@ import Input from "@/components/Shared/Input";
 const Form = () => {
   const [isLoading, setIsLoading] = useState(false);
 
-  const [userData, setUserData] = useState({
-    district: "",
-    area: "",
-    description: "",
-
-    subDivision: "",
-  });
+  const [formData, setFormData] = useState({});
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
-    setUserData({
-      ...userData,
+    setFormData({
+      ...formData,
       [name]: value,
     });
   };
@@ -38,7 +32,7 @@ const Form = () => {
       style={{
         boxShadow: "0px 5px 43px 0px rgba(17, 29, 25, 0.12)",
       }}
-      className=" sm:p-10 px-4 py-7 w-full font-poppins bg-[#fff] "
+      className=" sm:p-10 px-4 w-[90%] mx-auto py-7  font-poppins bg-[#fff] "
     >
       <h1 className="font-bold text-2xl">Make a request</h1>
       <p className="text-sm mt-3 leading-6 text-[#62706b]">
@@ -46,80 +40,50 @@ const Form = () => {
         touch. Or you can call us and our specialists will provide help!
       </p>
       <form className="w-full  mt-10 " onSubmit={handleSubmit}>
-        <div className="grid mt-3 sm:mt-0 md:grid-cols-2 gap-5">
-          <div>
-            <label
-              htmlFor="district-select"
-              className="font-semibold text-sm text-[#202725] mb-1"
-            >
-              Select Your District
-            </label>
-
-            <div className="relative inline-block w-full cursor-pointer">
-              <select
-                id="district-select"
-                name="district"
-                required
-                value={userData.district}
-                onChange={handleInputChange}
-                className="outline-none appearance-none text-sm  p-4 w-full rounded-md border-2 border-[#d9e4df] "
-              >
-                <option value="">Select District</option>
-              </select>
-              <div className="pointer-events-none absolute inset-y-0 right-2 text-[16px] flex items-center px-2 text-[#202725]">
-                <FaChevronDown />
-              </div>
-            </div>
-          </div>
-
-          <div>
-            <label
-              htmlFor="district-select"
-              className="font-semibold text-sm text-[#202725] mb-1"
-            >
-              Select Your Sub Division
-            </label>
-            <div className="relative inline-block w-full cursor-pointer">
-              <select
-                id="subDivision-select"
-                required
-                name="subDivision"
-                value={userData.subDivision}
-                onChange={handleInputChange}
-                className="outline-none text-sm appearance-none  p-4 w-full rounded-md border-2 border-[#d9e4df]"
-                disabled={!userData.district}
-              >
-                <option value="">Select SubDivision</option>
-              </select>
-              <div className="pointer-events-none absolute inset-y-0 right-2 text-[16px] flex items-center px-2 text-[#202725]">
-                <FaChevronDown />
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div className="w-full mt-3">
+        <div className="w-full flex flex-col gap-y-3 mb-3">
           <Input
-            name="area"
-            label="Enter Your Area"
-            type="text"
+            name="credit_score"
+            label="Enter initial credit score."
+            type="number"
             required
-            value={userData.area}
-            placeholder="Please enter your area"
+            min={0}
+            placeholder="The initial credit score"
+            onChange={handleInputChange}
+          />
+
+          <Input
+            name="spending_habits"
+            label="Enter spending habits"
+            type="number"
+            required
+            min={0}
+            max={10}
+            placeholder="A score representing spending habits (0-10)"
             onChange={handleInputChange}
           />
         </div>
 
-        <div className="w-full my-3">
-          <p className="font-semibold text-sm text-[#202725] mb-1">
-            Write your query
-          </p>
-          <textarea
-            name="description"
-            onChange={handleInputChange}
-            value={userData.description}
+        <div className="w-full flex flex-col gap-y-3">
+          <Input
+            name="payment_history"
+            label="Enter payment history"
+            type="number"
             required
-            className="outline-none min-h-[150px] sm:min-h-[200px] text-sm  p-4 w-full rounded-md border-2 border-[#d9e4df] "
+            min={0}
+            max={100}
+            placeholder="Payment history percentage (0-100)"
+            onChange={handleInputChange}
+          />
+
+          <Input
+            name="debt_to_income_ratio"
+            label="Enter spend habits"
+            type="number"
+            required
+            min={0}
+            max={10}
+            placeholder="Debt-to-income ratio percentage"
+            onChange={handleInputChange}
           />
         </div>
 
